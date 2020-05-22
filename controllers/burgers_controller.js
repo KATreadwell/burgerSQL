589626dbd3bc2a5fea580c52d6c7c6dd53491a8a
KAT <katreadwell1@gmail.com>
2020-05-21 18:11:43 -0700

    added env variable to try to use heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,35 +1,39 @@
 let express = require("express");
 let router = express.Router();
-let db = require("../models/burger")
+let db = require("../models");
 
-module.exports = function(app) {
-    app.get("/api/burgers", function(req, req) {
-        db.Burger.findAll({}).then(function(dbBurger) {
+// module.exports = function (app) {
+
+// };
+
+router.get("/", function (req, res) {
+    console.log("got this far")
+    db.Burger.findAll({}).then(function (dbBurger) {
+        console.log("got this far 2")
+        res.json(dbBurger);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    db.Burger.create({
+        name: req.body.name,
+    }).then(function (dbBurger) {
+        res.json(dbBurger)
+    });
+});
+
+router.put("/api/burgers", function (req, res) {
+    db.Burger.update({
+        name: req.body.name,
+    }, {
+        where: {
+            id: req.body.id
+        }
+    })
+        .then(function (dbBurger) {
             res.json(dbBurger);
         });
-    });
-
-    app.post("/api/burgers", function(req, res) {
-        db.Burger.create({
-            name: req.body.name,
-        }).then(function(dbBurger){
-            res.json(dbBurger)
-        });
-    });
-
-    app.put("/api/burgers", function(req, res) {
-        db.Burger.update({
-            name: req.body.name,
-        }, {
-            where: {
-                id: req.body.id
-            }
-        })
-        .then(function(dbBurger) {
-            res.json(dbBurger);
-        });
-    });
-};
+});
 
 module.exports = router;
 
@@ -56,10 +60,10 @@ module.exports = router;
 // router.put("/api/burgers/:id", function(req, res){
 //     let cols = ["devoured"];
 //     let vals = [JSON.parse(req.body.devoured)];
- 
+
 //     burger.updateOne(
 //         cols, vals, req.params.id,
-        
+
 //         function(result) {
 //         if (result.changedRows == 0){
 //             return res.status(404).end();
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(routes);
 //     console.log("Server listening on: http://localhost:" + PORT);
 // });
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("Server listening on: http://localhost:" + PORT);
     });
